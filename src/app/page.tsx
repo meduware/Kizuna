7abc30e08a86fd94684500f02136e242bebd9080
Enes Ceylan <enesfurkanceylan@hotmail.com>
2024-09-22 00:25:55 +0300

    #4 - I made some changes in the grid layout system for mobil view.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import Navbar from "@/components/(mainpage)/Navbar/Navbar";
 export default function Home() {
   return (
     <main className="flex flex-col items-center w-full">
-      <div className="absolute inset-0 bg-[url(/grid.svg)] [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))] -z-50 h-[800px]" />
+      {/* <div className="absolute inset-0 bg-[url(/grid.svg)] [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))] -z-50 h-[800px]" /> */}
       <Navbar />
       <Hero />
       <About />
