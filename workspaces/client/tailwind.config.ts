0f1125f0e7c287aa9869011bea8c84b65e6e6866
Salih Akyüzoğlu <sakyuzogluh@gmail.com>
2025-03-03 03:38:53 +0300

    feat:customize Shadcn/UI Sidebar-07 according to project requirements. (#22)

--- workspaces/client/tailwind.config.ts
+++ workspaces/client/tailwind.config.ts
@@ -10,111 +10,121 @@ const config = {
   ],
   prefix: "",
   theme: {
-    container: {
-      padding: '2rem',
-      screens: {
-        '2xl': '1400px'
-      }
-    },
-    extend: {
-      colors: {
-        border: 'hsl(var(--border))',
-        input: 'hsl(var(--input))',
-        ring: 'hsl(var(--ring))',
-        background: 'hsl(var(--background))',
-        foreground: 'hsl(var(--foreground))',
-        primary: {
-          DEFAULT: 'hsl(var(--primary))',
-          foreground: 'hsl(var(--primary-foreground))'
-        },
-        secondary: {
-          DEFAULT: 'hsl(var(--secondary))',
-          foreground: 'hsl(var(--secondary-foreground))'
-        },
-        destructive: {
-          DEFAULT: 'hsl(var(--destructive))',
-          foreground: 'hsl(var(--destructive-foreground))'
-        },
-        muted: {
-          DEFAULT: 'hsl(var(--muted))',
-          foreground: 'hsl(var(--muted-foreground))'
-        },
-        accent: {
-          DEFAULT: 'hsl(var(--accent))',
-          foreground: 'hsl(var(--accent-foreground))'
-        },
-        popover: {
-          DEFAULT: 'hsl(var(--popover))',
-          foreground: 'hsl(var(--popover-foreground))'
-        },
-        card: {
-          DEFAULT: 'hsl(var(--card))',
-          foreground: 'hsl(var(--card-foreground))'
-        }
-      },
-      borderRadius: {
-        lg: 'var(--radius)',
-        md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)'
-      },
-      keyframes: {
-        meteor: {
-          '0%': {
-            transform: 'rotate(215deg) translateX(0)',
-            opacity: '1'
-          },
-          '70%': {
-            opacity: '1'
-          },
-          '100%': {
-            transform: 'rotate(215deg) translateX(-500px)',
-            opacity: '0'
-          }
-        },
-        'border-beam': {
-          '100%': {
-            'offset-distance': '100%'
-          }
-        },
-        'spin-around': {
-          '0%': {
-            transform: 'translateZ(0) rotate(0)'
-          },
-          '15%, 35%': {
-            transform: 'translateZ(0) rotate(90deg)'
-          },
-          '65%, 85%': {
-            transform: 'translateZ(0) rotate(270deg)'
-          },
-          '100%': {
-            transform: 'translateZ(0) rotate(360deg)'
-          }
-        },
-        slide: {
-          to: {
-            transform: 'translate(calc(100cqw - 100%), 0)'
-          }
-        },
-        shine: {
-          '0%': {
-            'background-position': '0% 0%'
-          },
-          '50%': {
-            'background-position': '100% 100%'
-          },
-          to: {
-            'background-position': '0% 0%'
-          }
-        }
-      },
-      animation: {
-        meteor: 'meteor 5s linear infinite',
-        'border-beam': 'border-beam calc(var(--duration)*1s) infinite linear',
-        'spin-around': 'spin-around calc(var(--speed) * 2) infinite linear',
-        slide: 'slide var(--speed) ease-in-out infinite alternate',
-        shine: 'shine var(--duration) infinite linear'
-      }
-    }
+  	container: {
+  		padding: '2rem',
+  		screens: {
+  			'2xl': '1400px'
+  		}
+  	},
+  	extend: {
+  		colors: {
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			sidebar: {
+  				DEFAULT: 'hsl(var(--sidebar-background))',
+  				foreground: 'hsl(var(--sidebar-foreground))',
+  				primary: 'hsl(var(--sidebar-primary))',
+  				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
+  				accent: 'hsl(var(--sidebar-accent))',
+  				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
+  				border: 'hsl(var(--sidebar-border))',
+  				ring: 'hsl(var(--sidebar-ring))'
+  			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		keyframes: {
+  			meteor: {
+  				'0%': {
+  					transform: 'rotate(215deg) translateX(0)',
+  					opacity: '1'
+  				},
+  				'70%': {
+  					opacity: '1'
+  				},
+  				'100%': {
+  					transform: 'rotate(215deg) translateX(-500px)',
+  					opacity: '0'
+  				}
+  			},
+  			'border-beam': {
+  				'100%': {
+  					'offset-distance': '100%'
+  				}
+  			},
+  			'spin-around': {
+  				'0%': {
+  					transform: 'translateZ(0) rotate(0)'
+  				},
+  				'15%, 35%': {
+  					transform: 'translateZ(0) rotate(90deg)'
+  				},
+  				'65%, 85%': {
+  					transform: 'translateZ(0) rotate(270deg)'
+  				},
+  				'100%': {
+  					transform: 'translateZ(0) rotate(360deg)'
+  				}
+  			},
+  			slide: {
+  				to: {
+  					transform: 'translate(calc(100cqw - 100%), 0)'
+  				}
+  			},
+  			shine: {
+  				'0%': {
+  					'background-position': '0% 0%'
+  				},
+  				'50%': {
+  					'background-position': '100% 100%'
+  				},
+  				to: {
+  					'background-position': '0% 0%'
+  				}
+  			}
+  		},
+  		animation: {
+  			meteor: 'meteor 5s linear infinite',
+  			'border-beam': 'border-beam calc(var(--duration)*1s) infinite linear',
+  			'spin-around': 'spin-around calc(var(--speed) * 2) infinite linear',
+  			slide: 'slide var(--speed) ease-in-out infinite alternate',
+  			shine: 'shine var(--duration) infinite linear'
+  		}
+  	}
   },
   plugins: [require("tailwindcss-animate")],
 } satisfies Config
